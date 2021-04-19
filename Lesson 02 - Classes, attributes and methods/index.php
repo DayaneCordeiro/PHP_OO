c49b403aa975eef2d540cbd18a6417adb40154cc
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-04-18 21:08:01 -0300

    Lesson 01 and 02: Classes, attributes and methods

---
+++ Lesson 02 - Classes, attributes and methods/index.php
@@ -0,0 +1,16 @@
+<?php
+
+class Person {
+    public $name;
+    public $age;
+
+    public function talk() {
+        echo "My name is " . $this->name . " and I am " . $this->age . " years old.";
+    }
+}
+
+$person1 = new Person();
+
+$person1->name = "Dayane";
+$person1->age  = 25;
+$person1->falar();
