7811282cb3b931739d5a57b631f5c8e3a1c4a167
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-04-20 23:53:50 -0300

    Lesson 03: Getters and setters

---
+++ Lesson 03 - Getters and Setters/index.php
@@ -0,0 +1,39 @@
+<?php
+
+class Login {
+    private $email;
+    private $password;
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail($email) {
+        $valid_email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $this->email = $valid_email;
+    }
+    
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    public function log_into() {
+        if ($this->email == "test@test.com" && $this->password == "123456"):
+            echo "Logged in successfully.";
+        else:
+            echo "Invalid data";
+        endif;
+    }
+}
+
+$login1 = new Login();
+
+$login1->setEmail("test@test.com");
+$login1->setPassword("123456");
+$login1->log_into();
+
+echo $logar->getEmail();
