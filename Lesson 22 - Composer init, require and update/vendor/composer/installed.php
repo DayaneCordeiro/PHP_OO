b046f62250e0093993d00160e846bcffab37fa1f
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-04-29 11:53:36 -0300

    [Feature] Lesson 22 - Composer init, require and update

---
+++ Lesson 22 - Composer init, require and update/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '516aee717b185d8fd76be567e6ee127bbb7366af',
+    'name' => 'nodestudio/composer',
+  ),
+  'versions' => 
+  array (
+    'nodestudio/composer' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '516aee717b185d8fd76be567e6ee127bbb7366af',
+    ),
+    'pagseguro/pagseguro-php-sdk' => 
+    array (
+      'pretty_version' => '4.2.1',
+      'version' => '4.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e164aff66fa089eb79a80ce6b7413fe36aa9b95',
+    ),
+  ),
+);
