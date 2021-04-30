b8cf4dedca1d24d02bd4a4788fa5a65bb75e58c9
DayaneCordeiro <dayane.cordeirogs@gmail.com>
2021-04-29 22:22:57 -0300

    [Feature] Lesson 24 - CRUD with PDO, singleton and autoload #1

---
+++ Lesson 24 to 27 - CRUD with PDO, singleton and autoload/App/Model/Connection.php
@@ -0,0 +1,13 @@
+<?php
+
+class Connection {
+    private static $instance;
+
+    // Verify if alread exists a Connection instance
+    public static function getConn() {
+        if (!isset(self::$instance))
+            self::$instance = new PDO('mysql:host=localhost;dbname=pdo;charset=utf8', 'root', ''); // Connection URL; User; Pasword
+        else
+            return self::$instance;
+    }
+}
---
+++ Lesson 24 to 27 - CRUD with PDO, singleton and autoload/App/Model/ProductDao.php
@@ -0,0 +1,19 @@
+<?php
+
+class ProductDao {
+    public function create(Product $product) {
+        //
+    }
+
+    public function read() {
+        //
+    }
+
+    public function update(Product $product) {
+        //
+    }
+
+    public function delete($id) {
+        //
+    }
+}
